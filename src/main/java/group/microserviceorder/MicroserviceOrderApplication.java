bd18afa7c7ce178f5664f9e9987deddff2034d04
tysn07 <tyler0702@naver.com>
2024-10-12 00:53:31 +0900

    Initial Commit

---
+++ src/main/java/group/microserviceorder/MicroserviceOrderApplication.java
@@ -0,0 +1,13 @@
+package group.microserviceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group/microserviceorder/MicroserviceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package group.microserviceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
